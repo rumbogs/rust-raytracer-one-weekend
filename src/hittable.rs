1e5bebf55882eceab1812a232c68dfc2a1209497
Bogdan Pestritu <bogdan.pestritu@gmail.com>
2019-08-13 16:14:16 +0300

    test

--- src/hittable.rs
+++ src/hittable.rs
@@ -19,31 +19,12 @@ impl HitRecord {
     }
 }
 
+trait Hittable {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<(HitRecord, &Material)>;
+    fn bounding_box(&self, t0: f32, t1: f32) -> Option<Aabb>;
+}
+
 pub enum Object {
-    XYRect {
-        x0: f32,
-        x1: f32,
-        y0: f32,
-        y1: f32,
-        k: f32,
-        material: Material,
-    },
-    XZRect {
-        x0: f32,
-        x1: f32,
-        z0: f32,
-        z1: f32,
-        k: f32,
-        material: Material,
-    },
-    YZRect {
-        y0: f32,
-        y1: f32,
-        z0: f32,
-        z1: f32,
-        k: f32,
-        material: Material,
-    },
     Sphere {
         center: Vector3,
         radius: f32,
@@ -84,81 +65,6 @@ pub enum Object {
 impl Object {
     pub fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<(HitRecord, &Material)> {
         match self {
-            Object::XYRect {
-                x0,
-                x1,
-                y0,
-                y1,
-                k,
-                material,
-            } => {
-                let ray_origin = r.origin();
-                let ray_direction = r.direction();
-                let t: f32 = (k - ray_origin.z()) / r.direction().z();
-                if t < t_min || t > t_max {
-                    return None;
-                }
-                let x: f32 = ray_origin.x() + t * ray_direction.x();
-                let y: f32 = ray_origin.y() + t * ray_direction.y();
-                if x < *x0 || x > *x1 || y < *y0 || y > *y1 {
-                    return None;
-                }
-                let u = (x - x0) / (x1 - x0);
-                let v = (y - y0) / (y1 - y0);
-                let p = r.point_at_parameter(t);
-                let normal = Vector3::new(0.0, 0.0, 1.0);
-                Some((HitRecord::new(u, v, t, p, normal), &material))
-            }
-            Object::XZRect {
-                x0,
-                x1,
-                z0,
-                z1,
-                k,
-                material,
-            } => {
-                let ray_origin = r.origin();
-                let ray_direction = r.direction();
-                let t: f32 = (k - ray_origin.y()) / r.direction().y();
-                if t < t_min || t > t_max {
-                    return None;
-                }
-                let x: f32 = ray_origin.x() + t * ray_direction.x();
-                let z: f32 = ray_origin.z() + t * ray_direction.z();
-                if x < *x0 || x > *x1 || z < *z0 || z > *z1 {
-                    return None;
-                }
-                let u = (x - x0) / (x1 - x0);
-                let v = (z - z0) / (z1 - z0);
-                let p = r.point_at_parameter(t);
-                let normal = Vector3::new(0.0, 1.0, 0.0);
-                Some((HitRecord::new(u, v, t, p, normal), &material))
-            }
-            Object::YZRect {
-                y0,
-                y1,
-                z0,
-                z1,
-                k,
-                material,
-            } => {
-                let ray_origin = r.origin();
-                let ray_direction = r.direction();
-                let t: f32 = (k - ray_origin.x()) / r.direction().x();
-                if t < t_min || t > t_max {
-                    return None;
-                }
-                let y: f32 = ray_origin.y() + t * ray_direction.y();
-                let z: f32 = ray_origin.z() + t * ray_direction.z();
-                if y < *y0 || y > *y1 || z < *z0 || z > *z1 {
-                    return None;
-                }
-                let u = (y - y0) / (y1 - y0);
-                let v = (z - z0) / (z1 - z0);
-                let p = r.point_at_parameter(t);
-                let normal = Vector3::new(1.0, 0.0, 0.0);
-                Some((HitRecord::new(u, v, t, p, normal), &material))
-            }
             Object::Sphere {
                 center,
                 radius,
@@ -329,39 +235,6 @@ impl Object {
 
     pub fn bounding_box(&self, t0: f32, t1: f32) -> Option<Aabb> {
         match self {
-            Object::XYRect {
-                x0,
-                x1,
-                y0,
-                y1,
-                k,
-                material: _,
-            } => Some(Aabb::new(
-                Vector3::new(*x0, *y0, k - 0.0001),
-                Vector3::new(*x1, *y1, k + 0.0001),
-            )),
-            Object::XZRect {
-                x0,
-                x1,
-                z0,
-                z1,
-                k,
-                material: _,
-            } => Some(Aabb::new(
-                Vector3::new(*x0, k - 0.0001, *z0),
-                Vector3::new(*x1, k + 0.0001, *z1),
-            )),
-            Object::YZRect {
-                y0,
-                y1,
-                z0,
-                z1,
-                k,
-                material,
-            } => Some(Aabb::new(
-                Vector3::new(k - 0.0001, *y0, *z0),
-                Vector3::new(k + 0.0001, *y1, *z1),
-            )),
             Object::Sphere {
                 center,
                 radius,
---
+++ src/modifiers/mod.rs
@@ -0,0 +1,3 @@
+pub mod flip_normals;
+pub mod rotate;
+pub mod translate;
