6a74a5c808484d3c869868ad1eec5ca94fa7a07f
Bogdan Pestritu <bogdan.pestritu@gmail.com>
2019-08-19 18:50:03 +0300

    Add final scene

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,7 @@ use material::Material;
 use modifiers::flip_normals::FlipNormals;
 use modifiers::rotate::RotateY;
 use modifiers::translate::Translate;
+use objects::bvh_tree::BvhTree;
 use objects::constant_medium::ConstantMedium;
 use objects::cube::Cube;
 use objects::moving_sphere::MovingSphere;
@@ -365,6 +366,158 @@ fn random_scene2() -> Vec<Box<Hittable>> {
     object_list
 }
 
+fn final_scene() -> Vec<Box<Hittable>> {
+    let mut list: Vec<Box<Hittable>> = Vec::with_capacity(30);
+    let mut boxlist: Vec<Box<Hittable>> = Vec::with_capacity(10000);
+    let mut boxlist2: Vec<Box<Hittable>> = Vec::with_capacity(10000);
+    let mut rng = rand::thread_rng();
+    let nb: usize = 20;
+
+    let white: Material = Material::Lambertian {
+        albedo: Texture::ConstantTexture {
+            color: Vector3::new(0.73, 0.73, 0.73),
+        },
+    };
+    let ground: Material = Material::Lambertian {
+        albedo: Texture::ConstantTexture {
+            color: Vector3::new(0.48, 0.83, 0.53),
+        },
+    };
+    for i in 0..nb {
+        for j in 0..nb {
+            let w: f32 = 100.0;
+            let x0: f32 = -1000.0 + i as f32 * w;
+            let z0: f32 = -1000.0 + j as f32 * w;
+            let y0: f32 = 0.0;
+            let x1: f32 = x0 + w;
+            let y1: f32 = 100.0 * (rng.gen::<f32>() + 0.01);
+            let z1: f32 = z0 + w;
+            boxlist.push(Box::new(Cube::new(
+                Vector3::new(x0, y0, z0),
+                Vector3::new(x1, y1, z1),
+                ground.clone(),
+            )))
+        }
+    }
+    list.push(Box::new(BvhTree::new(boxlist, 0.0, 1.0)));
+
+    let light: Material = Material::DiffuseLight {
+        emit: Texture::ConstantTexture {
+            color: Vector3::new(7.0, 7.0, 7.0),
+        },
+    };
+    list.push(Box::new(XZRect {
+        x0: 123.0,
+        x1: 423.0,
+        z0: 147.0,
+        z1: 412.0,
+        k: 554.0,
+        material: light,
+    }));
+
+    let center: Vector3 = Vector3::new(400.0, 400.0, 200.0);
+    list.push(Box::new(MovingSphere {
+        center0: center,
+        center1: center + Vector3::new(30.0, 0.0, 0.0),
+        time0: 0.0,
+        time1: 1.0,
+        radius: 50.0,
+        material: Material::Lambertian {
+            albedo: Texture::ConstantTexture {
+                color: Vector3::new(0.7, 0.3, 0.1),
+            },
+        },
+    }));
+
+    list.push(Box::new(Sphere {
+        center: Vector3::new(260.0, 150.0, 45.0),
+        radius: 50.0,
+        material: Material::Dielectric { ref_idx: 1.5 },
+    }));
+    list.push(Box::new(Sphere {
+        center: Vector3::new(0.0, 150.0, 145.0),
+        radius: 50.0,
+        material: Material::Metal {
+            albedo: Texture::ConstantTexture {
+                color: Vector3::new(0.8, 0.8, 0.9),
+            },
+            fuzz: 10.0,
+        },
+    }));
+    list.push(Box::new(Sphere {
+        center: Vector3::new(360.0, 150.0, 145.0),
+        radius: 70.0,
+        material: Material::Dielectric { ref_idx: 1.5 },
+    }));
+
+    list.push(Box::new(ConstantMedium::new(
+        Box::new(Sphere {
+            center: Vector3::new(360.0, 150.0, 145.0),
+            radius: 70.0,
+            material: Material::Dielectric { ref_idx: 1.5 },
+        }),
+        0.2,
+        Texture::ConstantTexture {
+            color: Vector3::new(0.2, 0.4, 0.9),
+        },
+    )));
+    list.push(Box::new(ConstantMedium::new(
+        Box::new(Sphere {
+            center: Vector3::new(0.0, 0.0, 0.0),
+            radius: 5000.0,
+            material: Material::Dielectric { ref_idx: 1.5 },
+        }),
+        0.0001,
+        Texture::ConstantTexture {
+            color: Vector3::new(1.0, 1.0, 1.0),
+        },
+    )));
+
+    let img = image::open("pug.jpg").unwrap();
+
+    list.push(Box::new(Sphere {
+        center: Vector3::new(400.0, 200.0, 400.0),
+        radius: 100.0,
+        material: Material::Lambertian {
+            albedo: Texture::ImageTexture { img },
+        },
+    }));
+
+    list.push(Box::new(Sphere {
+        center: Vector3::new(220.0, 280.0, 300.0),
+        radius: 80.0,
+        material: Material::Lambertian {
+            albedo: Texture::NoiseTexture {
+                noise: Perlin::new(),
+                scale: 0.1,
+            },
+        },
+    }));
+
+    let ns: usize = 1000;
+    for _i in 0..ns {
+        boxlist2.push(Box::new(Sphere {
+            center: Vector3::new(
+                165.0 * rng.gen::<f32>(),
+                165.0 * rng.gen::<f32>(),
+                165.0 * rng.gen::<f32>(),
+            ),
+            radius: 10.0,
+            material: white.clone(),
+        }))
+    }
+
+    list.push(Box::new(Translate {
+        object: Box::new(RotateY::new(
+            Box::new(BvhTree::new(boxlist2, 0.0, 1.0)),
+            15.0,
+        )),
+        offset: Vector3::new(-100.0, 270.0, 395.0),
+    }));
+
+    list
+}
+
 fn random_in_unit_sphere() -> Vector3 {
     let mut p: Vector3;
     loop {
@@ -407,10 +560,10 @@ fn color(r: &Ray, world: &ObjectList, depth: usize) -> Vector3 {
 fn main() {
     let cpu_num = num_cpus::get() - 1; // leave some for the rest of the processes
     let now = Instant::now();
-    let width = 400;
-    let height = 400;
+    let width = 1024;
+    let height = 720;
     // larger makes blur/shadows/antialias smoother
-    let smoothness: u32 = 100;
+    let smoothness: u32 = 10000;
     // use one less thread for exact division
     // the last one will have less pixels to calculate
     let thread_rows = height / cpu_num + 1;
@@ -436,8 +589,19 @@ fn main() {
         0.0,
         1.0,
     );
+    let final_camera: &Camera = &Camera::new(
+        Vector3::new(478.0, 278.0, -600.0),
+        Vector3::new(278.0, 278.0, 0.0),
+        Vector3::new(0.0, 1.0, 0.0),
+        40.0,
+        width as f32 / height as f32,
+        0.0,
+        10.0,
+        0.0,
+        1.0,
+    );
 
-    let mut scene = cornell_smoke();
+    let mut scene = final_scene();
     // let balls = random_scene2();
     // scene.push(Box::new(create_binary_tree(balls, 0.0, 1.0)));
     let world = &ObjectList::new(scene);
@@ -476,7 +640,7 @@ fn main() {
                         for _ in 0..smoothness {
                             let u = (x as f32 + rng.gen::<f32>()) / width as f32;
                             let v = (inverted_row + rng.gen::<f32>()) / height as f32;
-                            let r = cornell_camera.get_ray(u, v);
+                            let r = final_camera.get_ray(u, v);
                             col += color(&r, &world, 0);
                         }
                         col /= smoothness as f32;
